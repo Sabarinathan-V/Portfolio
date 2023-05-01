5b2e60777a56f7f04fe9699150796a7db4d97eb0
sabarinathan <sabarivanam@gmail.com>
2023-05-01 23:10:24 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+let btn = document.querySelector('.toggle');
+let icon = document.querySelector('.toggle i');
+let body = document.querySelector('.hero-header');
+let image = document.getElementById('myImage');
+
+btn.onclick = function(){
+    body.classList.toggle('light-theme');
+
+    if(body.classList.contains('light-theme'))
+    {
+        icon.classList.add('fa-moon');
+        icon.classList.remove('fa-sun');
+        image.src = './images/white_bg.jpg'
+    }
+    else{
+        icon.classList.add('fa-sun');
+        icon.classList.remove('fa-moon');
+        image.src = './images/black_bg.jpg';
+    }
+};
+
+function openPdf(){
+    const fileUrl = './cv.pdf';
+    const win = window.open(fileUrl, '_black');
+    win.focus();
+}
